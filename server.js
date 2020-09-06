19634b7f342e6be55c5ad53551bb6b20c69657fd
Vishal <vishalshaw1998@gmail.com>
2020-09-06 16:03:31 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+});
+app.use("/peerjs", peerServer);
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World");
+});
+
+app.get("/:id", (req, res) => {
+    res.json({
+        status: "OK",
+    });
+});
+
+io.on("connection", (socket) => {
+    socket.on("join-room", (id, peerId) => {
+        socket.join(id);
+        socket.to(id).broadcast.emit("user-connected", peerId);
+    });
+});
+
+server.listen(3001);
